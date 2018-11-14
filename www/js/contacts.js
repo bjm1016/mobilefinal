bc9d217fb6398f84ebfd31b1d95ebc06dbb38108
Jon <ghostmij@gmail.com>
2018-11-14 10:17:26 -0500

    Update

--- www/js/contacts.js
+++ www/js/contacts.js
@@ -7,6 +7,7 @@ function onready
 
 function saveContact()
 {
+	contact = navigator.contacts.create();
 	contact.name = document.getElementById("firstName").value + " " + document.getElementById("lastName").value;
 	contact.save();
 }
