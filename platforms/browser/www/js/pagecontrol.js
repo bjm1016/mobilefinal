b7e8152aa379a1e9a35c4da2d565a3ca77c2c7f9
Jon <ghostmij@gmail.com>
2018-11-30 14:21:45 -0500

    Update

---
+++ platforms/browser/www/js/pagecontrol.js
@@ -0,0 +1,26 @@
+function changePage()
+{
+	var elementArray = document.getElementsByClassName("page");
+	for (i = 0; i < elementArray.length; i++)
+	{
+		if (this.innerHTML.toLowerCase() == elementArray[i].id)
+		{
+			elementArray[i].style.display = "block";
+		}
+		else
+		{
+			elementArray[i].style.display = "none";
+		}
+	}
+	closeMenu();
+}
+
+function openMenu()
+{
+	document.getElementById("navMenu").style.display = "block";
+}
+
+function closeMenu()
+{
+	document.getElementById("navMenu").style.display = "none";
+}
